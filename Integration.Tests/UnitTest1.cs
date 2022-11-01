4fddd78166ba8b14213c2c5f05472e4631b67f24
David Maltby <david.maltby@hotmail.com>
2022-11-01 13:46:30 -0500

    Unable to build when a sorting clause has been added.

--- Integration.Tests/UnitTest1.cs
+++ Integration.Tests/UnitTest1.cs
@@ -26,11 +26,11 @@ public class UnitTest1
         Assert.Equal("Scott Hanselman", noSortResponse.Data[0]);
         Assert.Equal("Jon Skeet", noSortResponse.Data[1]);
 
-        //var sortResponse = await qlClient.Query(static query => query.Authors(new AuthorSortInput[] { new AuthorSortInput { Name = SortEnumType.Asc } } , o => o.Name));
-        //Assert.Null(sortResponse.Errors);
-        //Assert.Equal(2, sortResponse.Data.Length);
-        //Assert.Equal("Jon Skeet", sortResponse.Data[0]);
-        //Assert.Equal("Scott Hanselman", sortResponse.Data[1]);
+        var sortResponse = await qlClient.Query(static query => query.Authors(new AuthorSortInput[] { new AuthorSortInput { Name = SortEnumType.Asc } }, o => o.Name));
+        Assert.Null(sortResponse.Errors);
+        Assert.Equal(2, sortResponse.Data.Length);
+        Assert.Equal("Jon Skeet", sortResponse.Data[0]);
+        Assert.Equal("Scott Hanselman", sortResponse.Data[1]);
 
     }
 }
